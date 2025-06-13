4cbe9114f27bdf81d6dd930c3dc8f7429ad32300
Edgar <edgaarr2005@hotmail.com>
2025-06-13 19:08:38 -0400

    Better set up for Coaches and Requests

--- src/pages/coaches/TheCoaches.vue
+++ src/pages/coaches/TheCoaches.vue
@@ -1,4 +1,23 @@
 <template>
-    <p>The Coaches</p>
+    <div class="container">
+    <section>
+        FILTER
+    </section>
+    <section>
+        LIST OF COACHES
+    </section>
+    </div>
 </template>
 <script></script>
+
+<style scoped>
+.container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 1rem;
+    padding: 2rem;
+    height: auto !important;
+}
+</style>
+
--- src/pages/requests/ViewRequests.vue
+++ src/pages/requests/ViewRequests.vue
@@ -1,4 +1,19 @@
 <template>
-    <p>View Requests</p>
+    <div class="container">
+    <section>
+        REQUESTS
+    </section>
+    </div>
 </template>
 <script></script>
+
+<style scoped>
+.container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 1rem;
+    padding: 2rem;
+    height: auto !important;
+}
+</style>
